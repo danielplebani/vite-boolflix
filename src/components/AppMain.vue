be366a668960aec0c28f796aecbaa7385fab9e0b
danielplebani <danielpleba03@gmail.com>
2023-10-02 17:31:32 +0200

    cast-- milestone 5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,108 +1,120 @@
 <script>
 import { state } from '../state.js';
 
-export default{
+export default {
+  name: 'AppMain',
 
-    name: 'AppMain',
+  data() {
+    return {
+      state,
+      overImage: null,
+    };
+  },
 
-    data(){
-      return{
-        state,
+  methods: {
+    showInfo(index) {
+      this.overImage = index;
+      const movie = this.state.movies[index];
 
-        overImage: null,
-      }
+      this.state.getCast(movie.id, movie.media_type)
+        .then((cast) => {
+          movie.cast = cast;
+        }
+      )
     },
-
-    methods: {
-      showInfo(index) {
-        this.overImage = index;
-      },
-      hideInfo(index) {
-        this.overImage = null;
-      },
+    
+    hideInfo(index) {
+      this.overImage = null;
     },
-  
-    created() {
-      state.connectAPI();
-    }
-}
+  },
 
+  created() {
+    this.state.connectAPI();
+  },
+};
 </script>
 
 
 <template>
   <div class="d-flex gap-2 flex-wrap pb-5">
-      <div class="position-relative" v-for="(movie, index) in state.movies">
-          <ul class="list-group" v-if="movie.poster_path">
+    <div class="position-relative" v-for="(movie, index) in state.movies" :key="index">
+      <ul class="list-group" v-if="movie.poster_path">
+        <img class="coverMovie" :src="'https://image.tmdb.org/t/p/w342' + movie.poster_path"
+          @mouseenter="showInfo(index)" @mouseleave="hideInfo(index)">
 
-               <img class="coverMovie" :src="'https://image.tmdb.org/t/p/w342' + movie.poster_path" @mouseenter="showInfo(index)" @mouseleave="hideInfo(index)">
-               
-               <div @mouseenter="showInfo(index)" @mouseleave="hideInfo(index)" class="info" v-if="overImage == index">
-                  <li class="list-group-item">
-                    <span v-if="movie.media_type == 'movie'"><strong>TITOLO:</strong> <br> {{movie.title}}</span>
-                    <span v-if="movie.media_type == 'tv'"><strong>TITOLO:</strong> <br> {{movie.name}}</span>
-                  </li>
-                  
-                  <li class="list-group-item">
-                    <span v-if="movie.media_type == 'movie'"><strong>TITOLO ORIGINALE:</strong> <br> {{movie.original_title}}</span>
-                    <span v-if="movie.media_type == 'tv' || movie.media_type == 'person'"><strong>TITOLO ORIGINALE:</strong> <br> {{movie.original_name}}</span>
-                  </li>
- 
-                  <li class="list-group-item" v-if="movie.overview"><strong>TRAMA:</strong> <br> {{movie.overview}}</li>
-                  
-                  <li class="list-group-item">
-                    <div class="d-flex align-items-center">
-                      <span class="me-2"><strong>LINGUA:</strong></span>
-                      <span v-if="!['en', 'es', 'fr', 'it', 'ja', 'tr'].includes(movie.original_language)">{{movie.original_language}}</span>
-                      <img class="flags" :src="state.languageFlags[movie.original_language]" v-if="['en', 'es', 'fr', 'it', 'ja', 'tr'].includes    (movie.original_language)">
-                    </div>
-                  </li>
-                  
-                  <li class="list-group-item">
-                    <div class="d-flex align-items-center">
-                      <span class="me-1"><strong>VOTO:</strong></span>
-                  
-                      <i v-for="i in Math.ceil(movie.vote_average / 2)" class="fa-solid fa-star text-warning"></i>
-                      <i v-for="i in (5 - Math.ceil((movie.vote_average / 2)))" class="fa-regular fa-star" v-if="movie.vote_average != 0"></i>
-                      <i v-for="i in 5" class="fa-regular fa-star" v-if="movie.vote_average == 0"></i>
-                    </div>
-                  </li>
-               </div>
-          </ul>
-      </div>
+        <div @mouseenter="showInfo(index)" @mouseleave="hideInfo(index)" class="info" v-if="overImage == index">
+          <li class="list-group-item">
+            <span v-if="movie.media_type == 'movie'"><strong>TITOLO:</strong> <br> {{ movie.title }}</span>
+            <span v-if="movie.media_type == 'tv'"><strong>TITOLO:</strong> <br> {{ movie.name }}</span>
+          </li>
+
+          <li class="list-group-item">
+            <span v-if="movie.media_type == 'movie'"><strong>TITOLO ORIGINALE:</strong> <br> {{ movie.original_title }}</span>
+            <span v-if="movie.media_type == 'tv' || movie.media_type == 'person'"><strong>TITOLO ORIGINALE:</strong> <br> {{ movie.original_name }}</span>
+          </li>
+
+          <li class="list-group-item" v-if="movie.overview"><strong>TRAMA:</strong> <br> {{ movie.overview }}</li>
+
+          <li class="list-group-item">
+            <div class="d-flex align-items-center">
+              <span class="me-2"><strong>LINGUA:</strong></span>
+              <span v-if="!['en', 'es', 'fr', 'it', 'ja', 'tr'].includes(movie.original_language)">{{ movie.original_language }}</span>
+              <img class="flags" :src="state.languageFlags[movie.original_language]"
+                v-if="['en', 'es', 'fr', 'it', 'ja', 'tr'].includes(movie.original_language)">
+            </div>
+          </li>
+
+          <li class="list-group-item">
+            <div class="d-flex align-items-center">
+              <span class="me-1"><strong>VOTO:</strong></span>
+              <i v-for="i in Math.ceil(movie.vote_average / 2)" class="fa-solid fa-star text-warning"></i>
+              <i v-for="i in (5 - Math.ceil((movie.vote_average / 2)))" class="fa-regular fa-star"
+                v-if="movie.vote_average != 0"></i>
+              <i v-for="i in 5" class="fa-regular fa-star" v-if="movie.vote_average == 0"></i>
+            </div>
+          </li>
+
+          <li class="list-group-item">
+            <strong>CAST:</strong> <br>
+            <ul>
+              <li v-for="actor in movie.cast">{{actor.name}}</li>
+            </ul>
+          </li>
+        </div>
+      </ul>
+    </div>
   </div>
-</template> 
-
+</template>
 
 
 <style lang="scss" scoped>
-.info{
-    width: 100%;
-    height: 100%;
-    overflow-y: auto;
-    position: absolute;
-    top: 0;
-    cursor: pointer;
+.info {
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  position: absolute;
+  top: 0;
+  cursor: pointer;
 
-    li{
-        background-color: rgba(0, 0, 0, 0.926);
-        color: white;
-        border: 0;
+  li {
+    background-color: rgba(0, 0, 0, 0.926);
+    color: white;
+    border: 0;
 
-        strong{
-            color: red;
-        }
+    strong {
+      color: red;
     }
+  }
 }
 
-.flags{
+.flags {
   width: 25px;
   height: 100%;
 }
 
-.coverMovie{
-    cursor: pointer;
-    width: 342px;
-    height: 513px;
+.coverMovie {
+  cursor: pointer;
+  width: 342px;
+  height: 513px;
 }
 </style>
--- src/state.js
+++ src/state.js
@@ -1,30 +1,49 @@
-import {reactive} from 'vue';
+// state.js
+import { reactive } from 'vue';
 import axios from 'axios';
 
 export const state = reactive({
-    base_url: 'https://api.themoviedb.org/3/search/multi?api_key=d7e5bf42a15a20ec8d10fa9ee57b36fe&include_adult=false&language=it-IT&page=1&query=a',
-    movies: null,
+  base_url: 'https://api.themoviedb.org/3/search/multi?api_key=d7e5bf42a15a20ec8d10fa9ee57b36fe&include_adult=false&language=it-IT&page=1&query=a',
+  cast_movie_url: 'https://api.themoviedb.org/3/movie/id/credits?api_key=d7e5bf42a15a20ec8d10fa9ee57b36fe',
+  cast_tv_url: 'https://api.themoviedb.org/3/tv/id/credits&api_key=d7e5bf42a15a20ec8d10fa9ee57b36fe',
 
-    
-    languageFlags: {
-      en: 'src/assets/img/Gran-bretagna.jpg',
-      es: 'src/assets/img/spagna.jpg',
-      fr: 'src/assets/img/Francia.jpg',
-      it: 'src/assets/img/ita.png',
-      ja: 'src/assets/img/bandiera-giappone.png',
-      tr: 'src/assets/img/ger.png'
-    },
-    
-    connectAPI(){
-      axios
-        .get(this.base_url)
-        .then(response => {
-          console.log(response.data.results);
-          this.movies = response.data.results;
-        })
-        .catch(error => {
-          console.log('Error:');
-          console.error(error);
-        })
-    }
-  })
+  movies: null,
+
+  languageFlags: {
+    en: 'src/assets/img/Gran-bretagna.jpg',
+    es: 'src/assets/img/spagna.jpg',
+    fr: 'src/assets/img/Francia.jpg',
+    it: 'src/assets/img/ita.png',
+    ja: 'src/assets/img/bandiera-giappone.png',
+    tr: 'src/assets/img/ger.png',
+  },
+
+  connectAPI() {
+    axios
+      .get(this.base_url)
+      .then((response) => {
+        console.log(response.data.results);
+        this.movies = response.data.results;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  getCast(id, mediaType) {
+    const castUrl =
+      mediaType === 'movie'
+        ? `https://api.themoviedb.org/3/movie/${id}/credits?api_key=d7e5bf42a15a20ec8d10fa9ee57b36fe`
+        : `https://api.themoviedb.org/3/tv/${id}/credits?api_key=d7e5bf42a15a20ec8d10fa9ee57b36fe`;
+
+    return axios
+      .get(castUrl)
+      .then((response) => {
+        return response.data.cast.slice(0, 5);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+});
+
